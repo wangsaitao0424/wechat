e6af962465a485f7c33ac62da3d94eed6ce467bb
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-14 16:21:22 +0800

    'event'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,6 +15,11 @@ class EventController extends Controller
     }
     public function event()
     {
-        echo $_GET['echostr'];
+        $info=file_get_contents("php://input");
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+        $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
+        $xml_arr=(array)$xml_obj;
+        dd($xml_arr);
     }
 }
