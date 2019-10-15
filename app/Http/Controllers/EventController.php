ac0a702c94e61e9993c902053a10500127423cea
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-15 11:01:50 +0800

    '1'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,7 +15,7 @@ class EventController extends Controller
     }
     public function event()
     {
-        echo $_GET['echostr'];die;
+//        echo $_GET['echostr'];die;
         $info=file_get_contents("php://input");
 //        dd($info);
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
