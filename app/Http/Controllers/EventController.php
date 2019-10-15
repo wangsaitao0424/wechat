466b1cafed054a446ede267b38294d6f2c216ec4
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-15 18:54:14 +0800

    'a'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -23,6 +23,11 @@ class EventController extends Controller
         $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);//解析xml 后面的两个参数是死的
         $xml_arr=(array)$xml_obj;//强行转化成数组
 //        dd($xml_arr);
-        var_dump($xml_arr);
+//        var_dump($xml_arr);
+//        普通消息
+        if($xml_arr['MsgType']=='text' && $xml_arr['Content']=='你好'){
+            echo '<xml><ToUserName><![CDATA['.$xml_arr['ToUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['FromUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>';
+        }
+
     }
 }
