c4a0f14db390e5ca18823ac2e55c0fd4a8f684e3
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-15 19:41:52 +0800

    'a'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,8 +26,9 @@ class EventController extends Controller
 //        var_dump($xml_arr);
 //        关注操作
         if($xml_arr['MsgType']=='event' && $xml_arr['Event']=="subscribe"){
-            $nickname=$this->tools->get_wechat_user($xml_arr['ToUserName']);
-            $msg='你好'.$nickname['$nickname'].',欢迎来到！';
+            $nickname=$this->tools->get_wechat_user($xml_arr['FromUserName']);
+//            dd($nickname);
+            $msg='你好'.$nickname["nickname"].',欢迎来到！';
             echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
         }
 //        普通消息
