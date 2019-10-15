17048fae6d155062cad4d383f1c73018eac2d559
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-15 15:52:21 +0800

    'a'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -16,13 +16,13 @@ class EventController extends Controller
     public function event()
     {
 //        echo $_GET['echostr'];die;
-        $info=file_get_contents("php://input");
+        $info=file_get_contents("php://input"); //接受  拿数据流用
 //        dd($info);
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
-        file_put_contents(storage_path('logs/wechat'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+        file_put_contents(storage_path('logs/wechat'.date('Y-m-d').'.log'),$info,FILE_APPEND);//  追加
 //        die();
-        $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
-        $xml_arr=(array)$xml_obj;
+        $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);//解析xml 后面的两个参数是死的
+        $xml_arr=(array)$xml_obj;//强行转化成数组
         dd($xml_arr);
     }
 }
