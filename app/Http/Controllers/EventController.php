ffccd34cba2c5ed95f873b97a8c8d6a26f70b545
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-15 19:08:37 +0800

    'b'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,9 +26,8 @@ class EventController extends Controller
 //        var_dump($xml_arr);
 //        普通消息
         if($xml_arr['MsgType']=='text' && $xml_arr['Content']=='111'){
-            $msg="你好！";
-            echo '<xml><ToUserName><![CDATA['.$xml_arr['ToUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['FromUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$msg.']]></Content></xml>';
+            $msg = '你好！';
+            echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
         }
-
     }
 }
