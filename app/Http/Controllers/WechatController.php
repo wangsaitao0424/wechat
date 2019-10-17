21861cb0dd41a39ea873852ce3faf38036390dea
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-17 15:41:09 +0800

    'n'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -120,29 +120,29 @@ class WechatController extends Controller
     {
         $url='https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->tools->access_token();
         $data=[
-            'button'=>[
-                'type'=>'click',
-                'name'=>'今日歌曲',
-                'key'=>'V1001_TODAY_MUSIC',
+            "button"=>[
+                "type"=>"click",
+                "name"=>"今日歌曲",
+                "key"=>"V1001_TODAY_MUSIC"
             ],
             [
-                'name'=>'菜单',
-                'sub_button'=>[
-                    'type'=>'view',
-                    'name'=>'搜索',
-                    'url'=>'http://www.soso.com/',
+                "name"=>"菜单",
+                "sub_button"=>[
+                    "type"=>"view",
+                    "name"=>"搜索",
+                    "url"=>"http://www.soso.com/"
                 ],
                 [
-                    'type'=>'miniprogram',
-                    'name'=>'wxa',
-                    'url'=>'http://mp.weixin.qq.com',
-                    'appid'=>'wx286b93c14bbf93aa',
-                    'pagepath'=>'pages/lunar/index',
+                    "type"=>"miniprogram",
+                    "name"=>"wxa",
+                    "url"=>"http://mp.weixin.qq.com",
+                    "appid"=>"wx286b93c14bbf93aa",
+                    "pagepath"=>"pages/lunar/index"
                 ],
                 [
-                    'type'=>'click',
-                    'name'=>'赞一下我们',
-                    'key'=>'V1001_GOOD'
+                    "type"=>"click",
+                    "name"=>'赞一下我们',
+                    "key"=>"V1001_GOOD"
                 ]
             ]
         ];
