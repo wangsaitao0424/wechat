8009b95f7203b2c278604a22e7e771152a05d6f6
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-21 19:29:11 +0800

    'ss'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -59,7 +59,7 @@ class EventController extends Controller
                         'subscribe_time'=>$nickname['subscribe_time']
                     ]);
                 }
-                $today_time=data('Y-m-d',strtotime('-1',time()));
+                $today_time=date('Y-m-d',strtotime('-1',time()));
                 dd($today_time);
                 $integral_times=$integral_time['integral_time'];
                 dd($integral_times);
