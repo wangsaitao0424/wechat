00513103971b4a89fd86cde6e92d47d9f0c0baf7
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-24 09:11:31 +0800

    'a'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,11 +22,8 @@ class EventController extends Controller
         $info=file_get_contents("php://input"); //接受  拿数据流用
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);//  追加
-//        die();
         $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);//解析xml 后面的两个参数是死的
         $xml_arr=(array)$xml_obj;//强行转化成数组
-//        dd($xml_arr);
-//        var_dump($xml_arr);
 //        关注操作
         if($xml_arr['MsgType']=='event' && $xml_arr['Event']=='subscribe'){
             //判断openid表是否有当前openid
@@ -66,82 +63,77 @@ class EventController extends Controller
             }
             echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
         }
-        //签到
-        if($xml_arr['MsgType']=='event' && $xml_arr['Event'] == 'CLICK'){
-            //判断是否点的是签到
-            if($xml_arr['EventKey'] == 'aaaa'){
-                //查库
-                $integral_time=Sign::where(['openid'=>$xml_arr['FromUserName']])->first();
-//                dd(empty($integral_time));
-                //判断库中有没有该用户的信息
-                if(empty($integral_time)){
-                    //无 添加
-//                    dd(11);
-                    $nickname=$this->tools->get_wechat_user($xml_arr['FromUserName']);
-                    Sign::create([
-                        'openid'=>$nickname['openid'],
-                        'nickname'=>$nickname['nickname'],
-                        'sex'=>$nickname['sex'],
-                        'subscribe_time'=>$nickname['subscribe_time']
-                    ]);
-                }
-//                $ti=strtotime("-2 days");
-//                dd($ti);
-                //今天时间
-                $today_time=date('Y-m-d',time());
-                //昨天时间
-                $yesterday_time=date('Y-m-d',strtotime("-1 days"));
-//                dd($yesterday_time);
-                //签到的时间
-                $integral_times=date('Y-m-d',$integral_time['integral_time']);
-                //今日已签到
-                if($integral_times == $today_time){
-                    $msg='今日已签到 ';
-                    echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
-                }else{
-                    //今日未签到
-                    //连续签到
-                    if($yesterday_time == $integral_times){
-                        //超过5天按第一次计算
-                        if($integral_time['count'] >= 5){
-                            Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
-                                'integral'=>$integral_time['integral']+5,
-                                'integral_time'=>time(),
-                                'count'=>1
-                            ]);
-                        }else{
-                            //没超过5天
-                            Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
-                                'integral'=>$integral_time['integral']+($integral_time['count']+1)*5,
-                                'integral_time'=>time(),
-                                'count'=>$integral_time['count']+1
-                            ]);
-                        }
-                    }else{
-                        //没连续签到
-                        Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
-                            'integral'=>$integral_time['integral']+5,
-                            'integral_time'=>time(),
-                            'count'=>1
-                        ]);
-                    }
-                    $msg='签到成功';
-                    echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
-                }
-            }else{
-                //查询总积分
-                $integral_time=Sign::where(['openid'=>$xml_arr['FromUserName']])->first();
-                $msg='你的总积分为：'.$integral_time['integral'].'分';
-                echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
-            }
-
-        }
-        //课程管理
-//        if($xml_arr['MsgType']=='event' && $xml_arr['Event'] == 'VIEW'){
-//            if($xml_arr['EventKey']=='http://47.93.253.39/wechat/course/'){
-//
+//        //签到
+//        if($xml_arr['MsgType']=='event' && $xml_arr['Event'] == 'CLICK'){
+//            //判断是否点的是签到
+//            if($xml_arr['EventKey'] == 'aaaa'){
+//                //查库
+//                $integral_time=Sign::where(['openid'=>$xml_arr['FromUserName']])->first();
+////                dd(empty($integral_time));
+//                //判断库中有没有该用户的信息
+//                if(empty($integral_time)){
+//                    //无 添加
+////                    dd(11);
+//                    $nickname=$this->tools->get_wechat_user($xml_arr['FromUserName']);
+//                    Sign::create([
+//                        'openid'=>$nickname['openid'],
+//                        'nickname'=>$nickname['nickname'],
+//                        'sex'=>$nickname['sex'],
+//                        'subscribe_time'=>$nickname['subscribe_time']
+//                    ]);
+//                }
+////                $ti=strtotime("-2 days");
+////                dd($ti);
+//                //今天时间
+//                $today_time=date('Y-m-d',time());
+//                //昨天时间
+//                $yesterday_time=date('Y-m-d',strtotime("-1 days"));
+////                dd($yesterday_time);
+//                //签到的时间
+//                $integral_times=date('Y-m-d',$integral_time['integral_time']);
+//                //今日已签到
+//                if($integral_times == $today_time){
+//                    $msg='今日已签到 ';
+//                    echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+//                }else{
+//                    //今日未签到
+//                    //连续签到
+//                    if($yesterday_time == $integral_times){
+//                        //超过5天按第一次计算
+//                        if($integral_time['count'] >= 5){
+//                            Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
+//                                'integral'=>$integral_time['integral']+5,
+//                                'integral_time'=>time(),
+//                                'count'=>1
+//                            ]);
+//                        }else{
+//                            //没超过5天
+//                            Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
+//                                'integral'=>$integral_time['integral']+($integral_time['count']+1)*5,
+//                                'integral_time'=>time(),
+//                                'count'=>$integral_time['count']+1
+//                            ]);
+//                        }
+//                    }else{
+//                        //没连续签到
+//                        Sign::where(['openid'=>$xml_arr['FromUserName']])->update([
+//                            'integral'=>$integral_time['integral']+5,
+//                            'integral_time'=>time(),
+//                            'count'=>1
+//                        ]);
+//                    }
+//                    $msg='签到成功';
+//                    echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+//                }
+//            }else{
+//                //查询总积分
+//                $integral_time=Sign::where(['openid'=>$xml_arr['FromUserName']])->first();
+//                $msg='你的总积分为：'.$integral_time['integral'].'分';
+//                echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
 //            }
+//
 //        }
+        //课程管理
 //        普通消息
         if($xml_arr['MsgType']=='text' && $xml_arr['Content']=="你好"){
             $msg="你好！";
