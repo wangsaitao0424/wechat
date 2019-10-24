12794711ae81bb04d9557b76f046ecab5fe69b46
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-24 11:32:32 +0800

    'a'

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -42,6 +42,7 @@ class CourseController extends Controller
     {
         $uid=Request()->session()->get('uid');
         $cousr=Course::where(['uid'=>$uid])->first();
+        dd($cousr);
         return view('Course.courseUpdate',['course'=>$cousr]);
     }
     public function course_update_do()
