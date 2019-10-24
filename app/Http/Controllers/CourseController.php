6324b0290ff86d2517d26df831c78c4e559da44a
wangsaitao0424 <wangsaitao0424@163.com>
2019-10-24 09:46:03 +0800

    'a'

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,6 +9,7 @@ class CourseController extends Controller
     public function course()
     {
         $redirect_uri=env('APP_URL').'/wecaht/code';
+        dd($redirect_uri);
         $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('APPID').'&redirect_uri='.urlencode($redirect_uri) .'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 //        dd($url);
         header('Location:'.$url);
